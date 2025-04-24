98b6db7b625a11c783e09dbf5ed66953b1bc207d
Israel Mendoza <mbi1204@hotmail.com>
2025-04-24 15:21:36 -0600

    Update cdk_demo-stack.ts

--- lib/cdk_demo-stack.ts
+++ lib/cdk_demo-stack.ts
@@ -17,7 +17,7 @@ export class CdkDemoStack extends cdk.Stack {
     const myFunction = new lambda.Function(this, "fnDivisas", {
      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
      handler: "index.handler",   
-     code: lambda.Code.fromAsset("./lambda/prueba"),
+     code: lambda.Code.fromAsset("lambda/prueba"),
    });
    
   }
