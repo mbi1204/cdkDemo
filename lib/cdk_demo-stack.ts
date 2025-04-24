bee7c444f0ee21c4fa4c9e7168ef58eaaa27fe15
Israel Mendoza <mbi1204@hotmail.com>
2025-04-24 15:19:50 -0600

    lamda

--- lib/cdk_demo-stack.ts
+++ lib/cdk_demo-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDemoStack extends cdk.Stack {
@@ -9,20 +11,13 @@ export class CdkDemoStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-     
+    
     
     // Define the Lambda function resource
     const myFunction = new lambda.Function(this, "fnDivisas", {
      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
-     handler: "index.handler",
-     code: lambda.Code.fromInline(`
-       exports.handler = async function(event) {
-         return {
-           statusCode: 200,
-           body: JSON.stringify('Hello World!'),
-         };
-       };
-     `),
+     handler: "index.handler",   
+     code: lambda.Code.fromAsset("./lambda/prueba"),
    });
    
   }
